331a25d6da369ac3eee58e48553da21e4db9a809
Tomat <xxlennygamerxx@gmail.com>
2023-07-06 16:56:58 -0700

    Fix ACRV chunk oversight.

--- src/Tomat.GameMaker.IFF/Chunks/ARV/GameMakerAcrvChunk.cs
+++ src/Tomat.GameMaker.IFF/Chunks/ARV/GameMakerAcrvChunk.cs
@@ -1,4 +1,5 @@
-﻿using Tomat.GameMaker.IFF.DataTypes;
+﻿using System.IO;
+using Tomat.GameMaker.IFF.DataTypes;
 using Tomat.GameMaker.IFF.DataTypes.Models.AnimationCurve;
 
 namespace Tomat.GameMaker.IFF.Chunks.ARV;
@@ -11,11 +12,16 @@ public sealed class GameMakerAcrvChunk : AbstractChunk {
     public GameMakerAcrvChunk(string name, int size) : base(name, size) { }
 
     public override void Read(DeserializationContext context) {
+        var chunkVersion = context.Reader.ReadInt32();
+        if (chunkVersion != 1)
+            throw new InvalidDataException($"Expected chunk version 1, got {chunkVersion}.");
+
         AnimationCurves = new GameMakerPointerList<GameMakerAnimationCurve>();
         AnimationCurves.Read(context);
     }
 
     public override void Write(SerializationContext context) {
+        context.Writer.Write(1);
         AnimationCurves!.Write(context);
     }
 }
