0325d9406cb67e95e8de2d575da3fedd18952f96
Tomat <xxlennygamerxx@gmail.com>
2023-07-11 03:18:04 -0700

    Add EMBI chunk.

---
+++ src/Tomat.GameMaker.IFF/Chunks/EMBI/GameMakerEmbiChunk.cs
@@ -0,0 +1,27 @@
+﻿using System.IO;
+using Tomat.GameMaker.IFF.DataTypes;
+using Tomat.GameMaker.IFF.DataTypes.Models.EmbeddedImage;
+
+namespace Tomat.GameMaker.IFF.Chunks.EMBI;
+
+public sealed class GameMakerEmbiChunk : AbstractChunk {
+    public const string NAME = "EMBI";
+
+    public GameMakerList<GameMakerEmbeddedImage>? EmbeddedImages { get; set; }
+
+    public GameMakerEmbiChunk(string name, int size) : base(name, size) { }
+
+    public override void Read(DeserializationContext context) {
+        var chunkVersion = context.Reader.ReadInt32();
+        if (chunkVersion != 1)
+            throw new InvalidDataException("Expected chunk version 1, got " + chunkVersion);
+
+        EmbeddedImages = new GameMakerList<GameMakerEmbeddedImage>();
+        EmbeddedImages.Read(context);
+    }
+
+    public override void Write(SerializationContext context) {
+        context.Writer.Write(1);
+        EmbeddedImages?.Write(context);
+    }
+}
---
+++ src/Tomat.GameMaker.IFF/DataTypes/Models/EmbeddedImage/GameMakerEmbeddedImage.cs
@@ -0,0 +1,21 @@
+﻿using Tomat.GameMaker.IFF.Chunks;
+using Tomat.GameMaker.IFF.DataTypes.Models.String;
+using Tomat.GameMaker.IFF.DataTypes.Models.Texture;
+
+namespace Tomat.GameMaker.IFF.DataTypes.Models.EmbeddedImage;
+
+public sealed class GameMakerEmbeddedImage : IGameMakerSerializable {
+    public GameMakerPointer<GameMakerString> Name { get; set; }
+
+    public GameMakerPointer<GameMakerTextureItem> TextureItem { get; set; }
+
+    public void Read(DeserializationContext context) {
+        Name = context.ReadPointerAndObject<GameMakerString>();
+        TextureItem = context.ReadPointerAndObject<GameMakerTextureItem>();
+    }
+
+    public void Write(SerializationContext context) {
+        context.Writer.Write(Name);
+        context.Writer.Write(TextureItem);
+    }
+}
