bb30671942e2103765e8038de63521a17de7809b
Tomat <xxlennygamerxx@gmail.com>
2023-07-16 23:14:06 -0700

    eurgh

--- src/Tomat.GameBreaker/Views/MainWindow.cs
+++ src/Tomat.GameBreaker/Views/MainWindow.cs
@@ -14,11 +14,33 @@ public class MainWindow : Gtk.ApplicationWindow {
         SetTitle(this.controller.WindowTitle);
         SetDefaultSize(this.controller.DefaultWidth, this.controller.DefaultHeight);
 
-        var actionAbout = Gio.SimpleAction.New("about", null);
-        actionAbout.OnActivate += AboutOnActivate;
-        AddAction(actionAbout);
+        var aboutAction = Gio.SimpleAction.New("about", null);
+        aboutAction.OnActivate += AboutOnActivate;
+        AddAction(aboutAction);
         this.application.SetAccelsForAction("win.about", new[] { "F1" });
 
+        // Header bar
+        var headerBar = Adw.HeaderBar.New();
+        headerBar.TitleWidget = Adw.WindowTitle.New(this.controller.WindowTitle, "");
+
+        var menuButton = Gtk.MenuButton.New();
+        menuButton.IconName = "view-more-symbolic";
+        var menuPopover = Gtk.PopoverMenu.NewFromModel(null);
+        //var menuBox = Gtk.Box.New(Gtk.Orientation.Vertical, 0);
+        //var aboutMenuItem = Gtk.Button.NewWithLabel("About");
+        //aboutMenuItem.OnClicked += (_, _) => actionAbout.Activate(null);
+        //menuBox.Append(aboutMenuItem);
+        //menuPopover.Child = (menuBox);
+        var aboutShortcutController = Gtk.ShortcutController.New();
+        var aboutShortcut = Gtk.Shortcut.New(Gtk.ShortcutTrigger.ParseString("F1"), Gtk.ShortcutAction.ParseString("win.about"));
+        aboutShortcutController.AddShortcut(aboutShortcut);
+        //menuPopover.AddChild(aboutShortcutController, "about");
+        menuButton.Popover = menuPopover;
+        headerBar.PackEnd(menuButton);
+
+        SetTitlebar(headerBar);
+
+        // Window contents
         var box = Gtk.Box.New(Gtk.Orientation.Vertical, 0);
 
         var projectsHeader = Gtk.Label.New("Projects");
@@ -30,8 +52,6 @@ public class MainWindow : Gtk.ApplicationWindow {
         projectsHeader.SetMarginEnd(10);
         projectsHeader.SetMarginTop(10);
         projectsHeader.SetMarginBottom(10);
-        var attributes = Pango.FontDescription;
-        projectsHeader.SetAttributes();
         box.Append(projectsHeader);
 
         SetChild(box);
