bb192cc2254dfbba77a0ebae57d83a29a198485f
Tomat <xxlennygamerxx@gmail.com>
2023-07-29 00:09:53 -0700

    More appropriate property name.

--- gamemaker/src/Tomat.GameMaker.IFF/DataTypes/Models/DebugScript/GameMakerDebugScript.cs
+++ gamemaker/src/Tomat.GameMaker.IFF/DataTypes/Models/DebugScript/GameMakerDebugScript.cs
@@ -4,13 +4,13 @@ using Tomat.GameMaker.IFF.DataTypes.Models.String;
 namespace Tomat.GameMaker.IFF.DataTypes.Models.DebugScript;
 
 public sealed class GameMakerDebugScript : IGameMakerSerializable {
-    public GameMakerPointer<GameMakerString> Name { get; set; }
+    public GameMakerPointer<GameMakerString> Source { get; set; }
 
     public void Read(DeserializationContext context) {
-        Name = context.ReadPointerAndObject<GameMakerString>();
+        Source = context.ReadPointerAndObject<GameMakerString>();
     }
 
     public void Write(SerializationContext context) {
-        context.Write(Name);
+        context.Write(Source);
     }
 }
