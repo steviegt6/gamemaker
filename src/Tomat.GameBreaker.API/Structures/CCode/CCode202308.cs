b15aa3d021a7e59c1059f3fa297998e3387b29eb
Tomat <xxlennygamerxx@gmail.com>
2023-08-19 01:28:16 -0700

    dude it is so over

---
+++ src/Tomat.GameBreaker.API/Structures/CCode/CCode202308.cs
@@ -0,0 +1,24 @@
+﻿namespace Tomat.GameBreaker.API.Structures.CCode;
+
+public unsafe struct CCode202308 : ICCode {
+    private delegate*<int>* vptr;
+    private CCode202308* next;
+    private int kind;
+    private int compiled;
+    private char* str;
+    private RToken token;
+    private RValue value;
+    private VMBuffer* vm;
+    private VMBuffer* vmDebugInfo;
+    private char* code;
+    private char* name;
+    private int codeIndex;
+    private YYGMLFuncs* func;
+    private bool watch;
+    private int offset;
+    private int locals;
+    private int args;
+    private int flags;
+    private YYObjectBase* prototype;
+    
+}
---
+++ src/Tomat.GameBreaker.API/Structures/RValue.cs
@@ -0,0 +1,24 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Tomat.GameBreaker.API.Structures; 
+
+public unsafe struct RValue {
+    [FieldOffset(0)]
+    private double real;
+    
+    [FieldOffset(0)]
+    private int i32;
+    
+    [FieldOffset(0)]
+    private long i64;
+    
+    [FieldOffset(0)]
+    private YyObjectBase* @object;
+    
+    [FieldOffset(nint.Size)]
+    private CInstance* instance;
+    private RefString* @string;
+    private CDynamicArrayRef<RValue> embeddedArray;
+    private RefDynamicArrayOfRValue* refArray;
+    private void* pointer;
+}
