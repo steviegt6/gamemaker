c5a0056c91043f1462a24939f1989cf23ffbd907
Tomat <xxlennygamerxx@gmail.com>
2023-08-19 16:14:11 -0700

    Support path layer kind, begin setting up project for transpilation.

---
+++ src/Tomat.GameBreaker.Compiler.CSharp/CodeCompilation.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Mono.Cecil;
+using Tomat.GameMaker.IFF;
+using Tomat.GameMaker.IFF.Chunks;
+using Tomat.GameMaker.IFF.Chunks.GEN8;
+
+namespace Tomat.GameBreaker.Compiler.CSharp;
+
+public class CodeCompilation {
+    private readonly DeserializationContext context;
+
+    public GameMakerIffFile Iff => context.IffFile;
+
+    public CodeCompilation(DeserializationContext context) {
+        this.context = context;
+    }
+
+    public AssemblyDefinition Compile() {
+        var name = Iff.GetChunk<IGen8Chunk>().DisplayName.ExpectObject().Value;
+        var asmNameDef = new AssemblyNameDefinition(name, new Version(1, 0, 0, 0));
+        var asmDef = AssemblyDefinition.CreateAssembly(asmNameDef, name, ModuleKind.Dll);
+
+        return asmDef;
+    }
+}
