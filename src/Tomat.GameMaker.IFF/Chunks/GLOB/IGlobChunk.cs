30824c1f55c99c255419115c2f8cd8716995ef55
Tomat <xxlennygamerxx@gmail.com>
2023-09-10 22:36:28 -0700

    Specs for GLOB and GMEN.

--- src/Tomat.GameMaker.IFF/Chunks/GLOB/IGlobChunk.cs
+++ src/Tomat.GameMaker.IFF/Chunks/GLOB/IGlobChunk.cs
@@ -2,6 +2,13 @@
 
 namespace Tomat.GameMaker.IFF.Chunks.GLOB;
 
+// chunk GLOB {
+//     int32 numGlobalCode;
+//     array {
+//         int32 globalCode;
+//     } globalCode;
+// }
+
 public interface IGlobChunk : IGameMakerChunk {
     List<int> GlobalCodeIds { get; }
 }
--- src/Tomat.GameMaker.IFF/Chunks/GMEN/IGmenChunk.cs
+++ src/Tomat.GameMaker.IFF/Chunks/GMEN/IGmenChunk.cs
@@ -2,6 +2,13 @@
 
 namespace Tomat.GameMaker.IFF.Chunks.GMEN;
 
+// chunk GMEN {
+//     int32 numEndCode;
+//     array {
+//         int32 endCode;
+//     } endCode;
+// }
+
 /// <summary>
 ///     The <c>GMEN</c> chunk, which contains the game's end code entries.
 /// </summary>
