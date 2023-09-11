304ef6a8226062f53f602059bf148532c87f7464
Tomat <xxlennygamerxx@gmail.com>
2023-09-10 22:38:31 -0700

    Insane.

--- src/Tomat.GameMaker.IFF/Chunks/PSPS/IPspsChunk.cs
+++ src/Tomat.GameMaker.IFF/Chunks/PSPS/IPspsChunk.cs
@@ -2,6 +2,10 @@
 
 namespace Tomat.GameMaker.IFF.Chunks.PSPS;
 
+// chunk PSPS {
+//     byte[16] passcode;
+// }
+
 public interface IPspsChunk : IGameMakerChunk {
     Memory<byte> Passcode { get; set; }
 }
