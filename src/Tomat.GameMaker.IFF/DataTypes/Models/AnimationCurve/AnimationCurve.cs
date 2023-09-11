591c01f1adca2b30fa25ee0d404d70e3c72790d7
Tomat <xxlennygamerxx@gmail.com>
2023-09-10 22:17:27 -0700

    Assuming we don't care about anim curve names...

--- src/Tomat.GameMaker.IFF/DataTypes/Models/AnimationCurve/AnimationCurve.cs
+++ src/Tomat.GameMaker.IFF/DataTypes/Models/AnimationCurve/AnimationCurve.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections.Generic;
 using Tomat.GameMaker.IFF.Chunks;
 
-namespace Tomat.GameMaker.IFF.DataTypes.Models.AnimationCurve; 
+namespace Tomat.GameMaker.IFF.DataTypes.Models.AnimationCurve;
 
 /* model GMAnimationCurve {
  * #if false // TODO: I don't believe names are ever actually included...
@@ -24,33 +24,18 @@ public interface IAnimationCurve : IGameMakerSerializable {
 }
 
 internal sealed class GameMakerAnimationCurve : IAnimationCurve {
-    // public GameMakerPointer<GameMakerString> Name { get; set; }
-
     public AnimationCurveGraphType GraphType { get; set; }
 
     public int ChannelCount => Channels.Count;
 
     public List<IAnimationCurveChannel> Channels { get; set; } = null!;
 
-    // private readonly bool includeName;
-
-    /*public GameMakerAnimationCurve() : this(true) { }
-
-    public GameMakerAnimationCurve(bool includeName) {
-        this.includeName = includeName;
-    }*/
-
     public void Read(DeserializationContext context) {
-        //Name = includeName ? context.ReadPointerAndObject<GameMakerString>() : GameMakerPointer<GameMakerString>.NULL;
-
         GraphType = (AnimationCurveGraphType)context.ReadInt32();
         Channels = context.ReadList<IAnimationCurveChannel, GameMakerAnimationCurveChannel>();
     }
 
     public void Write(SerializationContext context) {
-        // if (includeName)
-        //     context.Write(Name);
-
         context.Write((int)GraphType);
         context.WriteList(Channels);
     }
