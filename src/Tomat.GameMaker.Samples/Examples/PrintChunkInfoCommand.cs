15fff2809fad89c6717c14560046b8c5959e406e
Tomat <xxlennygamerxx@gmail.com>
2023-09-10 22:39:32 -0700

    Been meaning to do this for a while.

--- src/Tomat.GameMaker.Samples/Examples/PrintChunkInfoCommand.cs
+++ src/Tomat.GameMaker.Samples/Examples/PrintChunkInfoCommand.cs
@@ -15,12 +15,7 @@ public sealed class PrintChunkInfoCommand : BaseCommand {
 
         using var stream = File.Open(WadPath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
         var wad = GameMakerIffFile.FromStream(stream, out _);
-        if (wad.Form is null)
-            throw new Exception("FORM chunk is null!");
-
         var form = wad.Form;
-        if (form.Chunks is null)
-            throw new Exception("FORM chunk's sub-chunks are null!");
 
         console.Output.WriteLine($"Chunk \"{form.Name}\" ({form.GetType().FullName})");
         console.Output.WriteLine($"Size: {form.Size} bytes");
