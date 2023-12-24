dd8d4dfe28f27e65c14ef951e850ae0df84233e0
tomat <xxlennygamerxx@gmail.com>
2023-12-24 15:49:36 -0800

    Add icon.

--- src/Tomat.GameBreaker/GameBreakerApplication.cs
+++ src/Tomat.GameBreaker/GameBreakerApplication.cs
@@ -1,4 +1,8 @@
-﻿using Silk.NET.Windowing;
+﻿using System;
+using Silk.NET.Core;
+using Silk.NET.Windowing;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
 using Tomat.GameBreaker.Features.Splash;
 using Tomat.GameBreaker.Windowing;
 
@@ -10,5 +14,20 @@ internal sealed class GameBreakerApplication : Application {
             WindowOptions.Default,
             (ref WindowOptions options) => new SplashWindow(ref options)
         );
+    }
+
+    protected override unsafe T InitializeWindow<T>(WindowOptions windowOptions, WindowFactory<T> windowFactory) {
+        var window = base.InitializeWindow(windowOptions, windowFactory);
+
+        window.Window.Load += () => {
+            using var stream = typeof(GameBreakerApplication).Assembly.GetManifestResourceStream("Tomat.GameBreaker.resources.icon.png")!;
+            using var image = Image.Load<Rgba32>(stream);
+            var bytes = new byte[image.Width * image.Height * sizeof(Rgba32)];
+            image.CopyPixelDataTo(bytes);
+            var rawImg = new RawImage(image.Width, image.Height, new Memory<byte>(bytes));
+            window.Window.SetWindowIcon(ref rawImg);
+        };
+
+        return window;
     }
 }
